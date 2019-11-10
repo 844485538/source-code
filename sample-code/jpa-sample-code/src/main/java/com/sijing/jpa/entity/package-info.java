b4e9e018fbb00a3ec3e53943dedf4cea27e9a15e
844485538 <844485538@qq.com>
2019-11-10 16:54:46 +0800

    remove sample code

--- sample-code/jpa-sample-code/src/main/java/com/sijing/jpa/entity/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 定义数据库表映射的实体类
- */
-/**
- * @author li
- *
- */
-package com.sijing.jpa.entity;
--- sample-code/jpa-sample-code/src/main/java/com/sijing/jpa/service/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 逻辑层
- */
-/**
- * @author li
- *
- */
-package com.sijing.jpa.service;
